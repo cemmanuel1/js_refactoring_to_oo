f5d6eeddb4d897c5bc135e73ba2b01687a35dfe9
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-07 19:47:45 -0700

    completed

--- application.js
+++ application.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
+  var newDie = new Die;
+  $('#roller button.add').on('click', newDie.addDie);
 
   $('#roller button.roll').on('click', function() {
     $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
+      newDie.roll();
+      newDie.changeValue($(die),newDie.value);
     });
   });
 });
+
+// //initialize the value as starting at 0
+function Die() {
+  this.value = 0;
+}
+
+Die.prototype.roll = function(){
+  this.value = Math.floor((Math.random()*6)+1);
+};
+
+Die.prototype.changeValue = function(die,value){
+  die.text(value);
+};
+
+Die.prototype.addDie = function(){
+  $('.dice').append('<div class="die">0</div>');
+};
